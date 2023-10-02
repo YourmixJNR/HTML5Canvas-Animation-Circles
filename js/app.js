ac3084064fd639516f9df96d2249f4828edc99ef
Michael Victor <radmakson@gmail.com>
2023-10-02 15:56:05 +0100

    comment js

--- js/app.js
+++ js/app.js
@@ -1,35 +1,44 @@
+// Declare variables for canvas and 2D rendering context
 var c;
 var ctx;
 
-function init(){
-    c = document.getElementById("myCanvas");
-    ctx = c.getContext("2d");
-    draw();
+// Initialize the canvas and context
+function init() {
+    c = document.getElementById("myCanvas"); // Get the canvas element by ID
+    ctx = c.getContext("2d"); // Get the 2D rendering context
+    draw(); // Call the draw function
 }
 
-function draw(){
+// Function to draw animated shapes
+function draw() {
     var posX = 0;
     var posY = 0;
-    setInterval(function(){
-        posX += 1;
-        posY += 1;
+    
+    // Set up a recurring animation using setInterval
+    setInterval(function() {
+        posX += 1; // Increment X position
+        posY += 1; // Increment Y position
         
-        ctx.fillStyle="black";
-        ctx.fillRect(0,0,c.width,c.height);
+        // Clear the canvas by drawing a black rectangle
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, c.width, c.height);
         
-        ctx.fillStyle="white";
+        // Draw a white circle with position changing on the X-axis
+        ctx.fillStyle = "white";
         ctx.beginPath();
-        ctx.arc(posX,120,55,0,Math.PI * 2);
+        ctx.arc(posX, 120, 55, 0, Math.PI * 2);
         ctx.fill();
         
-        ctx.fillStyle="red";
+        // Draw a red circle with position changing on the Y-axis
+        ctx.fillStyle = "red";
         ctx.beginPath();
-        ctx.arc(150,posY,78,0,Math.PI * 2);
+        ctx.arc(150, posY, 78, 0, Math.PI * 2);
         ctx.fill();
         
-        ctx.fillStyle="blue";
+        // Draw a blue circle with position changing on the Y-axis
+        ctx.fillStyle = "blue";
         ctx.beginPath();
-        ctx.arc(350,posY,38,0,Math.PI * 2);
+        ctx.arc(350, posY, 38, 0, Math.PI * 2);
         ctx.fill();
-    },4);
+    }, 4); // Execute the animation loop every 4 milliseconds
 }
